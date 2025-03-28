863eb0fc926a29594f240f565214029ae58375cc
Lily Paczesniak <paczesniak@google.com>
2025-03-28 13:49:09 +0000

    Integrate strapi into astro

---
+++ astro-test/src/interfaces/blogpost.ts
@@ -0,0 +1,9 @@
+export default interface BlogPost {
+    id: number;
+    title: string;
+    body_ckeditor: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  }
